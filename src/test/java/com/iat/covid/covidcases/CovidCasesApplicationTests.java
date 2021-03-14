43f2f81adaa3ff655528b0c7c78739979c3c2837
Ian Angelo Tappa <ianangelodtappa@live.com>
2021-03-15 04:44:13 +0800

    Initialize Spring Boot Project

---
+++ src/test/java/com/iat/covid/covidcases/CovidCasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iat.covid.covidcases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidCasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
